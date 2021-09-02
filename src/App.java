151d62fbbdab3fc77172f853196a9f32485f991f
daltonfranco <daltonpacolafranco.git@hotmail.com>
2021-09-02 14:24:07 -0300

    fibonacci sequence

---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Fibonacci fib = new Fibonacci();
+
+        fib.fibo(6);
+
+    }
+}
---
+++ src/Fibonacci.java
@@ -0,0 +1,24 @@
+public class Fibonacci{
+
+    public void fibo(int h){
+
+        int num1 = 0;
+        int num2 = 1;
+        int seq = 0;
+        int fibo = 0;
+
+        System.out.print(num1 + " -> ");
+        System.out.print(num2 + " -> ");
+
+        while(seq < h){
+            fibo = num1 + num2;
+            num1 = num2;
+            num2 = fibo;
+            System.out.print(fibo + " -> ");
+            seq++;
+            
+        }
+
+        System.out.println("End");
+    }
+}
